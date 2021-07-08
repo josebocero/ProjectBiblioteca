e7ce2418b9cf946e34975105f44d539d30f4f52e
josebocero <josebocero@gmail.com>
2021-07-08 23:16:18 +0200

    Initial commit

---
+++ app/src/main/java/jose_g_bocero/com/jose/projectbiblioteca/MainActivity.kt
@@ -0,0 +1,16 @@
+package jose_g_bocero.com.jose.projectbiblioteca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import jose_g_bocero.com.jose.library.ToolbarActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : ToolbarActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbarToLoad(toolbar as Toolbar)
+    }
+}
